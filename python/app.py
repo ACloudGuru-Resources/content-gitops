ead6dcd20a62f5cdc55b77b800d81c64f24f3ba8
computergrrl <44240149+computergrrl@users.noreply.github.com>
2022-08-29 09:09:57 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello A Cloud Guru lab test again!"
+    return "Hello A Cloud Guru!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
