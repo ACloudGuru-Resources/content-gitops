a3952fd2f8bb649cf81fae581355e85f0b5ded9d
Mohammed KHEZNADJI <33502768+GhassaneDz@users.noreply.github.com>
2022-09-18 22:24:25 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello MK World!"
+    return "Hello FGTECH World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
