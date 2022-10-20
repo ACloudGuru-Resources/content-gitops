b0aca7c73a8f177360f6c0562f8df22f475a2113
Gentjan Balo - MMS <53909184+ggt19@users.noreply.github.com>
2022-10-20 14:38:34 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Student Self Host 01!"
+    return "Hello Student Self Host 02!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
